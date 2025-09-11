096cf5103abd533a5a912c06dcbe6e898395af35
Lisa-624 <lisawanjiru624@gmail.com>
2025-09-11 19:06:41 +0300

    Update RecipeList.jsx to use Link from react-router-dom

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.filteredRecipes);
 
+  if (recipes.length === 0) return <p>No recipes found.</p>;
+
   return (
     <div>
       {recipes.map(recipe => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
