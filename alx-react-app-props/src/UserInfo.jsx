566aa3c78abdd6a119eb1938728d369e80bfc271
Lisa-624 <lisawanjiru624@gmail.com>
2025-09-12 11:57:43 +0300

    ALX-friendly Context API refactor: UserProfile consumes context directly

--- alx-react-app-props/src/UserInfo.jsx
+++
@@ -1,1 +0,0 @@
-import UserDetails from './UserDetails'; function UserInfo() { return <UserDetails />; } export default UserInfo;
--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,1 +1,1 @@
-import UserInfo from './UserInfo'; function UserProfile() { return <UserInfo />; } export default UserProfile;
+import { useContext } from 'react'; import UserContext from './UserContext'; function UserProfile() { const userData = useContext(UserContext); return (<div><p>Name: {userData.name}</p><p>Email: {userData.email}</p></div>);} export default UserProfile;
