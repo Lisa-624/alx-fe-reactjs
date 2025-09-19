9c0623efe2847a01eb0f90778cf50e86dae73e9c
Lisa-624 <lisawanjiru624@gmail.com>
2025-09-19 05:12:07 +0300

    chore: setup GitHub User Search React project (Task 0 complete)

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>GitHub User Search</h1>
+      <p>App is set up and ready for development.</p>
+    </div>
+  );
+};
+
+export default App;
+
